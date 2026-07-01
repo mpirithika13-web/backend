2bcbb5defbecac3a8e7671ef2c661d4209435201
Pirithika <mpirithika13@gmail.com>
2026-07-01 13:37:44 +0530

    Updated firstcommit

--- src/main/java/com/example/backend/service/AuthService.java
+++ src/main/java/com/example/backend/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.example.backend.service;
 import com.example.backend.model.User;
 import com.example.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
     @Autowired
+    
     private UserRepository userRepository;
     public User signup(User user) {
      return userRepository.save(user);
